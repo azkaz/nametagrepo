5fdb107aac2343fc3af69494ab79910403bf65a9
philip sjöholm <philip.sjoholm@gmail.com>
2017-05-30 14:25:13 +0200

    updated database connections and form

--- DBconnection.php
+++ DBconnection.php
@@ -1,3 +1,4 @@
+<meta charset="utf-8"/>
 <?php 
 	$servername = "localhost"; 
 	$username = "root"; 
@@ -10,16 +11,27 @@
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error); 
 	}
-	echo "Connected successfully";
 
 	/*
 	$sql = "INSERT INTO visitation (first_name, last_name, course_name, start_date, end_date) VALUES ('Mölle', 'Nöff', 'In the grim future there is only WAAAAR!!!', '2017-05-29','2017-05-30')";
+	*/
+	
+	
+
+	$first_name	= $conn->real_escape_string($_REQUEST['first_name']); 
+	$last_name = $conn->real_escape_string($_REQUEST['last_name']);
+	$course_name = $conn->real_escape_string($_REQUEST['course_name']);
+	$start_date = $conn->real_escape_string($_REQUEST['start_date']);
+	$end_date = $conn->real_escape_string($_REQUEST['end_date']);
+
+	$sql = "INSERT INTO visitation (visitor_id ,first_name, last_name, course_name, start_date, end_date) VALUES (default,'$first_name','$last_name','$course_name','$start_date','$end_date')";
 
 	if($conn->query($sql)===TRUE){
-		echo "new record created successfully"; 
+		header("LOCATION: http://localhost/test.php");
+
 	}else {
-		echo "Error: " . $sql . <br> . $conn->error; 
-	}*/
+		echo "Error: " . $sql . $conn->error; 
+	}
 	$conn->close(); 
- 
+	 
  ?>
--- test.php
+++ test.php
@@ -1,12 +1,17 @@
+
 <html>
 <head>
+	<meta charset="utf-8"/>
 	<title>Welcome</title>
 </head>
 <body>
-<form action="welcome.php" method="post">
-	Name: <input type="text" name="name"><br>
-	E-mail: <input type="text" name="email"><br>
-	<input type="submit" name="">
+<form action="DBconnection.php" method="post" target="_self">
+	First Name: <input type="text" name="first_name"><br>
+	Last Name: <input type="text" name="last_name"><br>
+	Course: <input type="text" name="course_name"><br>
+	Start Date: <input type="date" name="start_date" placeholder="yyyy-mm-dd"><br>
+	End Date: <input type="date" name="end_date" placeholder="yyyy-mm-dd"><br>
+	<input type="submit" name="Submit">
 </form>
 </body>
 </html>
