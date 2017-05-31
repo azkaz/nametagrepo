560713b0182cd2246dcd12534d70df9541f95941
philip sjöholm <philip.sjoholm@gmail.com>
2017-05-31 13:38:12 +0200

    can echo nametags now

--- printnametag.php
+++ printnametag.php
@@ -10,31 +10,52 @@
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error); 
 	}
-?>
 
 
 
-
-<html>
-    <head>  
-        <meta charset="utf-8"/>
-    </head>
-<body>
-Här ska vi printa ut nametagsen genom föregående sida
-
-    <div>
-<?php
-        $sql = "SELECT visitor_id, first_name, last_name FROM visitation";
+        $name_tag = '';
+        $sql = "SELECT visitor_id, first_name, last_name, course_name, start_date, end_date, visitor_picture FROM visitation";
         $result = $conn->query($sql);
         
             if ($result->num_rows > 0) {
         // output data of each row
-         while($row = $result->fetch_assoc()) {
-         echo "id: " . $row["visitor_id"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
-    }
-} else {
+         while($row = $result->fetch_assoc()) 
+         {
+
+             // starting building a long string that is concatignated by using .= 
+             // then echo out that string in html code below
+             $name_tag .= '<br>';
+             $name_tag .= '<div>';
+             $name_tag .= '<p>'. $row["first_name"] .'</p>';
+             $name_tag .= '<p>'. $row["last_name"] .'</p>';
+             $name_tag .= '<p><b>'. $row["course_name"] .'</b></p>'; 
+             $name_tag .= '<p>'. $row["start_date"] .'</p>';
+             $name_tag .= '<p>'. $row["end_date"] .'</p>';
+             $name_tag .= '</div>';
+
+         }
+
+    } 
+    else {
     echo "0 results";
+}
+    $conn->close();
 ?>
-    </div>
+
+<!DOCTYPE html>
+<html>
+<title>TEST</title>
+<style>
+p{
+    font-size: 10px;
+}
+</style>
+<body>
+    <h1>Funkar det ?<h1>
+        <div>
+            <?php
+                echo $name_tag;
+            ?>
+        </div>
 </body>
 </html>
