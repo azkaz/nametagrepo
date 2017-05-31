8db839084d0430c15821d3d2ed07ea77ea77e8d9
philip sjöholm <philip.sjoholm@gmail.com>
2017-05-31 15:32:27 +0200

    new commit

--- test.php
+++ test.php
@@ -5,12 +5,13 @@
 	<title>Welcome</title>
 </head>
 <body>
-<form action="DBconnection.php" method="post" target="_self">
+<form action="DBconnection.php" method="post" target="_self" enctype="multiform/form-data">
 	First Name: <input type="text" name="first_name"><br>
 	Last Name: <input type="text" name="last_name"><br>
 	Course: <input type="text" name="course_name"><br>
 	Start Date: <input type="date" name="start_date" placeholder="yyyy-mm-dd"><br>
 	End Date: <input type="date" name="end_date" placeholder="yyyy-mm-dd"><br>
+	Photo: <input type="file" name="image" accept="image/*"> 
 	<input type="submit" name="Submit">
 </form>
 
