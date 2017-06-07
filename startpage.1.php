731ab22bc4c3d735ed429cb179f4cec0cc5a43ca
philip <philip.sjoholm@gmail.com>
2017-06-07 15:09:23 +0200

    added abillity to save canvas as jpg

--- startpage.1.php
+++ startpage.1.php
@@ -22,13 +22,30 @@
 	<input type="text" name="course_name">- Course: <br>
 	<input type="date" name="start_date" placeholder="yyyy-mm-dd">- Start Date: <br>
 	<input type="date" name="end_date" placeholder="yyyy-mm-dd">- End Date: <br>
-<input id="companyText" type="text" name="visitor_picture" > 
-	<div style="min-width:220px;max-height:277px">
-		<canvas id="snapshot" width="220px"height="277px"></canvas>
+<input id="companyText" type="text" name="visitor_picture" placeholder="vänligen välj loga nedanför" >- Company Logo: 
+	<div style="width:150px;height:150px">
+	<canvas id="snapshot" width="220px"height="277px"></canvas>
 	</div>
 	<input type="submit" name="Submit">
+	<image id="logotype" src="visitor_pictures/Angry_Marine.jpg" style="max-width:100%;max-height:100%"></image>
 </form>
 	
+<div class="container">
+  <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Logo
+    <span class="caret"></span></button>
+    <ul id="ddmID" class="dropdown-menu">
+      <li><a href="#">Angry_Marine</a></li>
+      <li><a href="#">Waagh</a></li>
+      <li><a href="#">wereBusy</a></li>
+			<li><a href="#">GodEmperor</a></li>
+			<li><a href="#">Tau</a></li>
+			<li><a href="#">Photo</a></li>
+    </ul>
+  </div>
+</div>
+
+
 
 <form action="printallcard.php">
     <input type="submit" name="print" value="Show all visitors" />
@@ -40,6 +57,14 @@
 
 <script>
 	$("#companyText").hide();
+	$('#ddmID li').on('click', function(){
+		if($('#companyText').val($(this).text()) ==="Photo"){
+
+		}else{
+			$('#companyText').val($(this).text("picture"+myIncrement+""));
+		}
+		$('#logotype').attr('src','visitor_pictures/'+$('#companyText').val()+'.jpg');
+	});
 </script>
 </body>
 </html>
---
+++ webcam.js
@@ -0,0 +1,41 @@
+function loadWebcam() {
+    document.getElementById("dl").addEventListener('click', dlCanvas, false);
+
+
+var player = document.getElementById('player');
+var snapshotCanvas = document.getElementById('snapshot');
+var captureButton = document.getElementById('capture');
+
+var handleSuccess = function (stream) {
+    // Attach the video stream to the video element and autoplay.
+    player.srcObject = stream;
+};
+
+captureButton.addEventListener('click', function () {
+    var context = snapshotCanvas.getContext('2d');
+    // Draw the video frame to the canvas.
+   // context.drawImage(player, 0, 0, snapshotCanvas.width,
+     //   snapshotCanvas.height);
+     context.drawImage(player,200,100,220,277,0,0,220,277,);
+});
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(handleSuccess);
+
+//  var canvas = document.getElementById("snapshot"); 
+//  var canvas = document.getElementById("snapshot");
+
+/* REGISTER DOWNLOAD HANDLER */
+/* Only convert the canvas to Data URL when the user clicks. 
+   This saves RAM and CPU ressources in case this feature is not required. */
+function dlCanvas() {
+    var dt = snapshotCanvas.toDataURL('image/jpg');
+    /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
+    dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+
+    /* In addition to <da>'s "download" attribute, you can define HTTP-style headers */
+    dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Caaaaaanvas.jpg');
+
+    this.href = dt;
+};
+}
